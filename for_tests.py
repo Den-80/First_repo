f82145bd6ea61de7dcef3440064c0d29f4df60ee
Denis <cldionis@gmail.com>
2024-04-24 16:29:37 +0300

    commit

--- for_tests.py
+++ for_tests.py
@@ -1,11 +1,29 @@
-def factorial(n):
-    if n < 2:
-        return 1
-    else:
-        return n * factorial(n - 1)
+from datetime import datetime
 
-def number_of_groups(n, k):
-    result = factorial(n)/(factorial(n-k)*factorial(k))
-    return print(int(result))
+users = [
+    {"name": "Bill Gates", "birthday": "1955.3.25"},
+    {"name": "Steve Jobs", "birthday": "1955.3.21"},
+    {"name": "Jinny Lee", "birthday": "1956.3.22"},
+    {"name": "Sarah Lee", "birthday": "1957.3.23"},
+    {"name": "Jonny Lee", "birthday": "1958.3.22"},
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"}
+]
 
-print(number_of_groups(50, 7))
+def string_to_date(date_string: str):
+    formatted_datetime = datetime.strptime(date_string, "%Y.%m.%d")
+    converted_date = datetime.date(formatted_datetime)
+    return converted_date
+
+
+def date_to_string(date):
+    date_string = date.strftime('%Y.%m.%d')
+    return date_string
+
+
+def get_upcoming_birthdays(list_users: list) -> list: 
+    for user in users:
+        user_name = user.get('name')
+        user_birthday = user.get('birthday')
+        user_birthday_datetime = string_to_date(user_birthday)
+        if user_birthday_datetime in 
